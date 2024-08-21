3cc6feba6284d0b04cbbd9dbb1d0475f0a90b80d
Manuel Miglioranza <mmiglioranza22@gmail.com>
2024-08-21 16:02:19 -0300

    fix whitespace

--- commands.sql
+++ commands.sql
@@ -1,6 +1,8 @@
 CREATE TABLE blogs (
-    id SERIAL PRIMARY KEY, author text,
-    url text NOT NULL, title text NOT NULL,
+    id SERIAL PRIMARY KEY, 
+		author text,
+    url text NOT NULL, 
+		title text NOT NULL,
     likes integer DEFAULT 0
 );
 
